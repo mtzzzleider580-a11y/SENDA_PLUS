51f0e5708a8e2d8ddfdbf7e25d1c7621d659377e
Leider maza taboada <mtzzzleider580@gmail.com>
2026-05-04 19:06:34 -0500

    form invitado casi listo faltan algunos cambios

--- SENDAPLUS/Clases/Evento.cs
+++ SENDAPLUS/Clases/Evento.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +8,30 @@ using System.Threading.Tasks;
 
 namespace SENDAPLUS
 {
-    internal class Evento
-    {
-        public class evento
-        {
-            public string Id { get; set; }
-        }
+    
+    public class Evento
+    {// Id de MongoDB (ObjectId)
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("nombreEvento")]
+        public string NombreEvento { get; set; }
+
+        [BsonElement("tipoEvento")]
+        public string TipoEvento { get; set; }
+
+        [BsonElement("fecha")]
+        public DateTime Fecha { get; set; }
+
+        [BsonElement("hora")]
+        public string Hora { get; set; }
+
+        [BsonElement("lugar")]
+        public string Lugar { get; set; }
+
+        [BsonElement("estado")]
+        public string Estado { get; set; }
     }
+    
 }
--- SENDAPLUS/FormInvitado.Designer.cs
+++ SENDAPLUS/FormInvitado.Designer.cs
@@ -28,14 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnConsultar = new System.Windows.Forms.Button();
-            this.lblFiltrarpormes = new MaterialSkin.Controls.MaterialLabel();
-            this.materialComboBox1 = new MaterialSkin.Controls.MaterialComboBox();
+            this.cmbMes = new MaterialSkin.Controls.MaterialComboBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnCerrarsesión = new MaterialSkin.Controls.MaterialButton();
             this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -43,87 +39,58 @@
             this.HoraFin = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Lugar = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCerrarsesión = new MaterialSkin.Controls.MaterialButton();
+            this.btnfiltrar = new MaterialSkin.Controls.MaterialButton();
+            this.btnConsultar = new MaterialSkin.Controls.MaterialButton();
+            this.btnDetalles = new MaterialSkin.Controls.MaterialButton();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(590, 75);
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 74);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 40);
+            this.label1.Size = new System.Drawing.Size(99, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // btnConsultar
+            // cmbMes
             // 
-            this.btnConsultar.Location = new System.Drawing.Point(1051, 202);
-            this.btnConsultar.Name = "btnConsultar";
-            this.btnConsultar.Size = new System.Drawing.Size(247, 53);
-            this.btnConsultar.TabIndex = 1;
-            this.btnConsultar.Text = "Consultar";
-            this.btnConsultar.UseVisualStyleBackColor = true;
-            // 
-            // lblFiltrarpormes
-            // 
-            this.lblFiltrarpormes.AutoSize = true;
-            this.lblFiltrarpormes.Depth = 0;
-            this.lblFiltrarpormes.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.lblFiltrarpormes.Location = new System.Drawing.Point(796, 222);
-            this.lblFiltrarpormes.MouseState = MaterialSkin.MouseState.HOVER;
-            this.lblFiltrarpormes.Name = "lblFiltrarpormes";
-            this.lblFiltrarpormes.Size = new System.Drawing.Size(107, 19);
-            this.lblFiltrarpormes.TabIndex = 2;
-            this.lblFiltrarpormes.Text = "Filtrar por mes:";
-            // 
-            // materialComboBox1
-            // 
-            this.materialComboBox1.AutoResize = false;
-            this.materialComboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.materialComboBox1.Depth = 0;
-            this.materialComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-            this.materialComboBox1.DropDownHeight = 174;
-            this.materialComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.materialComboBox1.DropDownWidth = 121;
-            this.materialComboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
-            this.materialComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialComboBox1.FormattingEnabled = true;
-            this.materialComboBox1.IntegralHeight = false;
-            this.materialComboBox1.ItemHeight = 43;
-            this.materialComboBox1.Items.AddRange(new object[] {
-            "Enero",
-            "Febrero",
-            "Marzo",
-            "Abril",
-            "Mayo",
-            "Junio",
-            "Julio",
-            "Agosto",
-            "Septiembre",
-            "Octubre",
-            "Noviembre",
-            "Diciembre"});
-            this.materialComboBox1.Location = new System.Drawing.Point(909, 202);
-            this.materialComboBox1.MaxDropDownItems = 4;
-            this.materialComboBox1.MouseState = MaterialSkin.MouseState.OUT;
-            this.materialComboBox1.Name = "materialComboBox1";
-            this.materialComboBox1.Size = new System.Drawing.Size(121, 49);
-            this.materialComboBox1.StartIndex = 0;
-            this.materialComboBox1.TabIndex = 3;
+            this.cmbMes.AutoResize = false;
+            this.cmbMes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.cmbMes.Depth = 0;
+            this.cmbMes.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.cmbMes.DropDownHeight = 174;
+            this.cmbMes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMes.DropDownWidth = 121;
+            this.cmbMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.cmbMes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cmbMes.FormattingEnabled = true;
+            this.cmbMes.IntegralHeight = false;
+            this.cmbMes.ItemHeight = 43;
+            this.cmbMes.Location = new System.Drawing.Point(1165, 192);
+            this.cmbMes.MaxDropDownItems = 4;
+            this.cmbMes.MouseState = MaterialSkin.MouseState.OUT;
+            this.cmbMes.Name = "cmbMes";
+            this.cmbMes.Size = new System.Drawing.Size(121, 49);
+            this.cmbMes.StartIndex = 0;
+            this.cmbMes.TabIndex = 3;
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Algerian", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle6;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Nombre,
@@ -133,14 +100,6 @@
             this.HoraFin,
             this.Lugar,
             this.Estado});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Algerian", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.Location = new System.Drawing.Point(11, 261);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
@@ -148,25 +107,6 @@
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(1364, 415);
             this.dataGridView1.TabIndex = 4;
-            // 
-            // btnCerrarsesión
-            // 
-            this.btnCerrarsesión.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnCerrarsesión.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
-            this.btnCerrarsesión.Depth = 0;
-            this.btnCerrarsesión.HighEmphasis = true;
-            this.btnCerrarsesión.Icon = null;
-            this.btnCerrarsesión.Location = new System.Drawing.Point(680, 710);
-            this.btnCerrarsesión.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.btnCerrarsesión.MouseState = MaterialSkin.MouseState.HOVER;
-            this.btnCerrarsesión.Name = "btnCerrarsesión";
-            this.btnCerrarsesión.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.btnCerrarsesión.Size = new System.Drawing.Size(131, 36);
-            this.btnCerrarsesión.TabIndex = 5;
-            this.btnCerrarsesión.Text = "Cerrar Sesión";
-            this.btnCerrarsesión.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            this.btnCerrarsesión.UseAccentColor = false;
-            this.btnCerrarsesión.UseVisualStyleBackColor = true;
             // 
             // Nombre
             // 
@@ -216,16 +156,97 @@
             this.Estado.MinimumWidth = 8;
             this.Estado.Name = "Estado";
             // 
+            // btnCerrarsesión
+            // 
+            this.btnCerrarsesión.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCerrarsesión.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnCerrarsesión.Depth = 0;
+            this.btnCerrarsesión.HighEmphasis = true;
+            this.btnCerrarsesión.Icon = null;
+            this.btnCerrarsesión.Location = new System.Drawing.Point(1219, 725);
+            this.btnCerrarsesión.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnCerrarsesión.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnCerrarsesión.Name = "btnCerrarsesión";
+            this.btnCerrarsesión.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnCerrarsesión.Size = new System.Drawing.Size(131, 36);
+            this.btnCerrarsesión.TabIndex = 5;
+            this.btnCerrarsesión.Text = "Cerrar Sesión";
+            this.btnCerrarsesión.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnCerrarsesión.UseAccentColor = false;
+            this.btnCerrarsesión.UseVisualStyleBackColor = true;
+            this.btnCerrarsesión.Click += new System.EventHandler(this.btnCerrarsesión_Click);
+            // 
+            // btnfiltrar
+            // 
+            this.btnfiltrar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnfiltrar.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnfiltrar.Depth = 0;
+            this.btnfiltrar.HighEmphasis = true;
+            this.btnfiltrar.Icon = null;
+            this.btnfiltrar.Location = new System.Drawing.Point(984, 205);
+            this.btnfiltrar.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnfiltrar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnfiltrar.Name = "btnfiltrar";
+            this.btnfiltrar.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnfiltrar.Size = new System.Drawing.Size(145, 36);
+            this.btnfiltrar.TabIndex = 6;
+            this.btnfiltrar.Text = "filtrar por mes";
+            this.btnfiltrar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnfiltrar.UseAccentColor = false;
+            this.btnfiltrar.UseVisualStyleBackColor = true;
+            this.btnfiltrar.Click += new System.EventHandler(this.btnfiltrar_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnConsultar.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnConsultar.Depth = 0;
+            this.btnConsultar.HighEmphasis = true;
+            this.btnConsultar.Icon = null;
+            this.btnConsultar.Location = new System.Drawing.Point(11, 205);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnConsultar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnConsultar.Size = new System.Drawing.Size(174, 36);
+            this.btnConsultar.TabIndex = 7;
+            this.btnConsultar.Text = "CONSULTAR EVENTOS";
+            this.btnConsultar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnConsultar.UseAccentColor = false;
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnDetalles
+            // 
+            this.btnDetalles.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnDetalles.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnDetalles.Depth = 0;
+            this.btnDetalles.HighEmphasis = true;
+            this.btnDetalles.Icon = null;
+            this.btnDetalles.Location = new System.Drawing.Point(626, 699);
+            this.btnDetalles.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnDetalles.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnDetalles.Name = "btnDetalles";
+            this.btnDetalles.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnDetalles.Size = new System.Drawing.Size(212, 36);
+            this.btnDetalles.TabIndex = 8;
+            this.btnDetalles.Text = "VER DETALLES DEL EVENTO";
+            this.btnDetalles.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnDetalles.UseAccentColor = false;
+            this.btnDetalles.UseVisualStyleBackColor = true;
+            this.btnDetalles.Click += new System.EventHandler(this.btnDetalles_Click);
+            // 
             // FormInvitado
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(23F, 40F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 34F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1386, 788);
+            this.Controls.Add(this.btnDetalles);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnfiltrar);
             this.Controls.Add(this.btnCerrarsesión);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.materialComboBox1);
-            this.Controls.Add(this.lblFiltrarpormes);
-            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.cmbMes);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Algerian", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
@@ -243,9 +264,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnConsultar;
-        private MaterialSkin.Controls.MaterialLabel lblFiltrarpormes;
-        private MaterialSkin.Controls.MaterialComboBox materialComboBox1;
+        private MaterialSkin.Controls.MaterialComboBox cmbMes;
         private System.Windows.Forms.DataGridView dataGridView1;
         private MaterialSkin.Controls.MaterialButton btnCerrarsesión;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
@@ -255,5 +274,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn HoraFin;
         private System.Windows.Forms.DataGridViewTextBoxColumn Lugar;
         private System.Windows.Forms.DataGridViewTextBoxColumn Estado;
+        private MaterialSkin.Controls.MaterialButton btnfiltrar;
+        private MaterialSkin.Controls.MaterialButton btnConsultar;
+        private MaterialSkin.Controls.MaterialButton btnDetalles;
     }
 }
